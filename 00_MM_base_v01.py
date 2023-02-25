a02a7339c1f69966cd304c985350e008801829e4
TCreagh0098 <125223853+TCreagh0098@users.noreply.github.com>
2023-02-25 23:08:53 +1300

    Add files via upload

--- 00_MM_base_v01.py
+++ 00_MM_base_v01.py
@@ -39,6 +39,23 @@ def num_check(question):
             print("Please enter an integer")
 
 
+# calculate the ticket price base on the age
+def calc_ticket_price(var_age):
+    # ticket is $7.50 for users under 16
+    if var_age < 16:
+        price = 7.50
+
+    # ticket is $10.50 for users between 16 and 64
+    elif var_age < 65:
+        price = 10.5
+
+    # ticket price is $6.50 for seniors (65+)
+    else:
+        price = 6.5
+
+    return price
+
+
 # main routine starts here
 
 # set maximum number of tickets below
@@ -71,6 +88,10 @@ while tickets_sold < MAX_TICKETS:
     else:
         print("?? That looks like a typo, please try again. ")
         continue
+
+    # calculate ticket cost
+    ticket_cost = calc_ticket_price(age)
+    print(f'Age: {age}, Ticket Price: ${ticket_cost:.2f}')
 
     tickets_sold += 1
 
